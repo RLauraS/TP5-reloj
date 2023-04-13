8183351ebff852cc30c5433b9ab5ad5967140b03
RLauraS <R_LauraS@yahoo.com.ar>
2023-04-13 01:29:22 -0300

    Se realizan las funciones para mostrar un reloj de 12 horas

--- js/app.js
+++ js/app.js
@@ -2,18 +2,23 @@ function obtenerFecha(){
     let fecha = document.querySelector('#fecha');
     let hora = document.querySelector('#hora');
     let fechaActual = new Date();
-    fechaActual.getDate(); //devuelve la fecha (nro de día del mes)
-    fechaActual.getDay();// devuelve el nro del día de la semana 0 es domingo, 1 lunes...
-    fechaActual.getMonth();//devuelve nro de mes, 0 es enero, diciembre sería 11
-    
-    // Array de días de la semana
-    const daysOfWeek = ["Domingo", "Lunes", "Martes", "Miércoles", "Jueves", "Viernes", "Sábado"];
-    
-    // Array de meses
-    const months = ["Enero", "Febrero", "Marzo", "Abril", "Mayo", "Junio", "Julio", "Agosto", "Septiembre", "Octubre", "Noviembre", "Diciembre"];
-    
-    fecha.innerHTML = `${daysOfWeek[fechaActual.getDay()]} ${fechaActual.getDate()} de ${months[fechaActual.getMonth()]} del ${fechaActual.getFullYear()}`
-    
-    hora.innerHTML=`${fechaActual.getHours()}:${fechaActual.getMinutes()}:${fechaActual.getSeconds()}`
+    fechaActual.getDate(); 
+    fechaActual.getDay();
+    fechaActual.getMonth();
+
+    const diasDeLaSemana = ["Domingo", "Lunes", "Martes", "Miércoles", "Jueves", "Viernes", "Sábado"];
+
+    const meses = ["Enero", "Febrero", "Marzo", "Abril", "Mayo", "Junio", "Julio", "Agosto", "Septiembre", "Octubre", "Noviembre", "Diciembre"];
+
+    // Añadimos la lógica condicional para agregar un cero a la izquierda si la hora, minutos o segundos son menores de 10.
+    let horas = fechaActual.getHours() < 10 ? `0${fechaActual.getHours()}` : fechaActual.getHours();
+    let minutos = fechaActual.getMinutes() < 10 ? `0${fechaActual.getMinutes()}` : fechaActual.getMinutes();
+    let segundos = fechaActual.getSeconds() < 10 ? `0${fechaActual.getSeconds()}` : fechaActual.getSeconds();
+
+    fecha.innerHTML = `${diasDeLaSemana[fechaActual.getDay()]} ${fechaActual.getDate()} de ${meses[fechaActual.getMonth()]} del ${fechaActual.getFullYear()}`;
+
+    // Concatenamos la hora, minutos y segundos con el formato "hh:mm:ss AM/PM"
+    hora.innerHTML = `${horas}:${minutos}:${segundos} ${fechaActual.getHours() < 12 ? 'AM' : 'PM'}`;
 }
-setInterval(obtenerFecha,1000);
+
+setInterval(obtenerFecha, 1000);
